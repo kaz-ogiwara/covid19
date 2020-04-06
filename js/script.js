e8cf673921a3816e26ba25d3fe142940d8e734d3
Tomonobu Saito <Tomonobu.Saito@gmail.com>
2020-04-06 16:25:14 +0900

    type:"logarithmic"

--- js/script.js
+++ js/script.js
@@ -200,6 +200,7 @@ const init = () => {
             }
           }],
           yAxes: [{
+            type: "logarithmic",
             location: "bottom",
             stacked: false,
             gridLines: {
