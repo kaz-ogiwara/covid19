017d83e6421f3303fcf65ea2b4b718570fc60dfd
荻原 和樹 / Kazuki OGIWARA <kaz-ogiwara@users.noreply.github.com>
2020-04-06 15:58:40 +0900

    Add information of prefecture detail

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 let gData;
 let gRegions = [];
+let gThresholds = {
+  carriers: 0,
+  discharged: 0,
+  deaths: 0,
+  pcrtested: 0
+};
 
 const LANG = $("html").attr("lang");
-const REGION_THRESHOLD = 10;
 const COLORS = {
   default: "#3DC",
   carriers: "#3DC",
@@ -11,9 +16,9 @@ const COLORS = {
   discharged: "#9FC",
   serious: "#FEA",
   pcrtested: "#4CD",
-  checking: "#abc",
   dark: "#399",
   selected: "#EC2",
+  checking: "#abc",
   gender: {
     f: "#FE9",
     m: "#2B9"
@@ -39,21 +44,21 @@ const LABELS = {
       checking: "確認中"
     },
     age: [
-      "10歳未満",
-      "10代",
-      "20代",
-      "30代",
-      "40代",
-      "50代",
-      "60代",
-      "70代",
-      "80代",
       "90代",
+      "80代",
+      "70代",
+      "60代",
+      "50代",
+      "40代",
+      "30代",
+      "20代",
+      "10代",
+      "10歳未満",
       "不明"
     ]
   },
   en: {
-    change: "DoD: ",
+    change: "Daily: ",
     unit: {
       carriers: "",
       cases: "",
@@ -71,20 +76,21 @@ const LABELS = {
       checking: "Checking"
     },
     age: [
-      "Under 10",
-      "10s",
-      "20s",
-      "30s",
-      "40s",
-      "50s",
-      "60s",
-      "70s",
-      "80s",
       "90s",
+      "80s",
+      "70s",
+      "60s",
+      "50s",
+      "40s",
+      "30s",
+      "20s",
+      "10s",
+      "Under 10",
       "Unknown"
     ]
   }
 };
+
 
 
 const init = () => {
@@ -92,42 +98,67 @@ const init = () => {
     return String(num).replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
   }
 
-  const drawTransitionBlocks = () => {
-    $(".transition-block").each(function(){
+  const capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+  }
+
+  const updateThresholds = () => {
+    const median = (values) => {
+      let sorted = values.sort((a, b) => a - b);
+      let is = [Math.floor(values.length / 2), Math.ceil(values.length / 2)];
+      return (sorted[is[0]] + sorted[is[1]]) / 2;
+    }
+
+    for (thType in gThresholds) {
+      let values = [];
+      let rows = gData["prefectures-data"][thType];
+      let latest = rows[rows.length - 1];
+      for (let i = 3; i < latest.length; i++) {
+        values.push(latest[i]);
+      }
+      gThresholds[thType] = median(values);
+    }
+  }
+
+  const drawLatestValue = ($box, latestValue, latestChange) => {
+    latestChange = addCommas(latestChange).toString();
+        if (latestChange.charAt(0) !== "-") latestChange = "+" + latestChange;
+    let $latest = $box.find(".latest");
+        $latest.find(".value").text(addCommas(latestValue));
+        $latest.find(".unit").text(LABELS[LANG].unit[$box.attr("code")]);
+        $latest.find(".type").text(capitalize($box.find(".switch[value=total]").text()));
+        $latest.find(".change").text(LABELS[LANG].change + " " + latestChange);
+  }
+
+  const drawTransitionBoxes = () => {
+    $(".transition").each(function(){
       let code = $(this).attr("code");
       drawTransitionChart($(this), code);
     });
   }
 
-  const drawTransitionChart = ($block, code) => {
-    let $chart = $block.find(".chart").empty().html("<canvas></canvas>");
+  const drawTransitionChart = ($box, code) => {
+    let $chart = $box.find(".chart").empty().html("<canvas></canvas>");
     let $canvas = $chart.find("canvas")[0];
-    let switchValue = $block.find(".switch.selected").attr("value");
+    let switchValue = $box.find(".switch.selected").attr("value");
 
     let rows = gData.transition[code];
-
     let latestValue = rows[rows.length - 1][3];
     let latestChange = latestValue - rows[rows.length - 2][3];
-        latestChange = addCommas(latestChange).toString();
-        if (latestChange.charAt(0) !== "-") latestChange = "+" + latestChange;
-    let $latest = $block.find(".latest");
-    $latest.find(".value").text(addCommas(latestValue));
-    $latest.find(".unit").text(LABELS[LANG].unit[code]);
-    $latest.find(".type").text($block.find(".switch[value=total]").text());
-    $latest.find(".change").text(LABELS[LANG].change + " " + latestChange);
+    drawLatestValue($box, latestValue, latestChange);
 
     let config = {
       type: "bar",
       data: {
         labels: [],
         datasets: [{
-          label: $block.find("h3:first").text(),
+          label: $box.find("h3:first").text(),
           backgroundColor: COLORS[code],
           data: []
         }]
       },
       options: {
-        aspectRatio: 1.8,
+        aspectRatio: 1.6,
         responsive: true,
         legend: {
           display: false
@@ -144,7 +175,7 @@ const init = () => {
               let dateTime = tooltipItem[0].xLabel + " " + "12:00";
               if (LANG === "ja") dateTime = dateTime + "時点";
               if (LANG === "en") dateTime = "As of " + dateTime;
-              let suffix = $block.find(".switch.selected").text();
+              let suffix = $box.find(".switch.selected").text();
               return dateTime + " " + suffix;
             },
             label: function(tooltipItem, data){
@@ -190,8 +221,8 @@ const init = () => {
       }
     };
 
-    if ($block.width() >= 400) {
-      config.options.aspectRatio = 2.4;
+    if ($box.width() >= 400) {
+      config.options.aspectRatio = 2.0;
     }
 
     rows.forEach(function(row, i){
@@ -209,23 +240,29 @@ const init = () => {
     window.myChart = new Chart(ctx, config);
   }
 
-  const getPrefColor = (value) => {
-    let ret = "rgba(90, 90, 90, 0.3)";
-    if (value >= 1)  ret = COLORS.dark;
-    if (value >= REGION_THRESHOLD) ret = COLORS.default;
+  const getPrefColor = (prefCode) => {
+    let type = $("#select-pref-type").val();
+    let ret = "rgba(90, 90, 90, 0.6)";
+    let value = gData["prefectures-data"][type][gData["prefectures-data"][type].length - 1][parseInt(prefCode) + 2];
+    if (value >= 1) {
+      ret = COLORS.dark;
+      if (gThresholds[type] === 0) ret = COLORS.default;
+    }
+    if (value >= gThresholds[type] && gThresholds[type] >= 1) ret = COLORS.default;
     return ret;
   }
 
   const drawJapanMap = () => {
+    $("#japan-map").empty();
     const WIDTH = $("#japan-map").width();
 
     let prefs = [];
-    gData.prefectures.forEach(function(pref, i){
+    gData["prefectures-map"].forEach(function(pref, i){
       prefs.push({
         code: pref.code,
-        jp: pref.jp,
+        jp: pref.ja,
         en: pref.en,
-        color: getPrefColor(pref.value),
+        color: getPrefColor(pref.code),
         hoverColor: COLORS.selected,
         prefectures: [pref.code]
       });
@@ -233,18 +270,18 @@ const init = () => {
 
     $("#japan-map").japanMap({
       areas: prefs,
+      selection: "prefecture",
       width: WIDTH,
-      borderLineColor: "#fcfcfc",
+      borderLineColor: "#242a3c",
       borderLineWidth : 0.25,
       lineColor : "#ccc",
       lineWidth: 1,
       drawsBoxLine: false,
       showsPrefectureName: false,
       movesIslands : true,
-      fontSize : 11,
-      onHover : function(data){
-        console.log("a");
-        drawRegionChart(data.code, 0);
+      onHover: function(data){
+        drawRegionChart(data.code);
+        drawPrefectureCharts(data.code);
       }
     });
   }
@@ -371,11 +408,10 @@ const init = () => {
     window.myChart = new Chart(ctx, config);
   }
 
-  const drawRegionChart = (targetRegion) => {
-    let $wrapper = $("#region-chart");
-    $wrapper.empty();
-    $wrapper.html('<canvas></canvas>');
+  const drawRegionChart = (prefCode) => {
+    let $wrapper = $("#region-chart").empty().html('<canvas></canvas>');
     let $canvas = $wrapper.find("canvas")[0];
+    let dataType = $("#select-pref-type").val();
 
     let config = {
       type: "horizontalBar",
@@ -442,18 +478,175 @@ const init = () => {
       }
     };
 
-    if ($wrapper.outerWidth() >= 400) config.options.aspectRatio = 0.4;
-    if (targetRegion !== "") config.options.animation.duration = 0;
+    if ($wrapper.outerWidth() >= 400) config.options.aspectRatio = 0.5;
+    if (prefCode !== "") config.options.animation.duration = 0;
 
-    gData.prefectures.forEach(function(pref, i){
-      if (pref.value >= 1) {
-        config.data.labels.push(pref[LANG]);
-        config.data.datasets[0].data.push(pref.value);
+    let prefs = [];
+    gData["prefectures-data"][dataType][gData["prefectures-data"][dataType].length - 1].forEach(function(value, i){
+      if (i >= 3) {
+        prefs.push({name:gData["prefectures-map"][i - 3][LANG], value:value, code:(i - 2).toString()});
+      }
+    });
 
-        if (targetRegion === pref.code) {
-          config.data.datasets[0].backgroundColor.push(COLORS.selected);
-        } else {
-          config.data.datasets[0].backgroundColor.push(getPrefColor(pref.value));
+    prefs.sort((a, b) => {
+      if(a.value < b.value) return 1;
+      if(a.value > b.value) return -1;
+      return 0;
+    });
+
+    prefs.forEach(function(pref, i){
+      config.data.labels.push(pref.name);
+      config.data.datasets[0].data.push(pref.value);
+
+      if (prefCode == pref.code) {
+        config.data.datasets[0].backgroundColor.push(COLORS.selected);
+      } else {
+        config.data.datasets[0].backgroundColor.push(getPrefColor(pref.code));
+      }
+    });
+
+    let ctx = $canvas.getContext('2d');
+    window.myChart = new Chart(ctx, config);
+  }
+
+  const drawPrefectureCharts = (prefCode) => {
+    $("#select-prefecture").val(prefCode);
+    $(".prefecture-chart").each(function(){
+      let code = $(this).attr("code");
+      drawPrefectureChart(prefCode, code);
+    });
+  }
+
+  const drawPrefectureChart = (prefCode, typeCode) => {
+    let $box = $(".prefecture-chart[code=" + typeCode + "]");
+    $box.find("h3").find("span").text(gData["prefectures-map"][parseInt(prefCode) - 1][LANG]);
+
+    let $wrapper = $box.find(".chart").empty().html('<canvas></canvas>');
+    let $canvas = $wrapper.find("canvas")[0];
+    let switchValue = $box.find(".switch.selected").attr("value");
+
+    let rows = gData["prefectures-data"][typeCode];
+    let latestValue = rows[rows.length - 1][parseInt(prefCode) + 2];
+    let latestChange = latestValue - rows[rows.length - 2][parseInt(prefCode) + 2];
+    drawLatestValue($box, latestValue, latestChange);
+
+    let config = {
+      type: "line",
+      data: {
+        labels: [],
+        datasets: []
+      },
+      options: {
+        aspectRatio: 1.6,
+        animation: {
+          duration: 1000
+        },
+        responsive: true,
+        legend: {
+          display: false
+        },
+        title: {
+          display: false
+        },
+        tooltips: {
+          xPadding: 24,
+          yPadding: 12,
+          mode: 'x',
+          displayColors: false,
+          callbacks: {
+            title: function(tooltipItem){
+              if (tooltipItem[0].datasetIndex === 0) {
+                return $box.find("h3").text();
+              }
+            },
+            label: function(tooltipItem, data){
+              if (tooltipItem.datasetIndex === 0) {
+                let suffix = {
+                  ja: " 名",
+                  en: " cases"
+                };
+                return tooltipItem.xLabel + " " + tooltipItem.yLabel + suffix[LANG];
+              }
+            }
+          }
+        },
+        scales: {
+          xAxes: [{
+            position: "bottom",
+            gridLines: {
+              display: false
+            },
+            ticks: {
+              suggestedMin: 0,
+              fontColor: "rgba(255,255,255,0.7)",
+              maxRotation: 0,
+              minRotation: 0,
+              callback: (label) => {
+                return " " + label + " ";
+              }
+            }
+          }],
+          yAxes: [{
+            gridLines: {
+              color: "rgba(255,255,255,0.2)"
+            },
+            ticks: {
+              fontColor: "rgba(255,255,255,0.7)",
+              callback: function(value, index, values) {
+                if (Math.floor(value) === value) {
+                  return addCommas(value.toString());
+                }
+              }
+            }
+          }]
+        }
+      }
+    };
+
+    if ($wrapper.outerWidth() >= 400) config.options.aspectRatio = 2.0;
+
+    config.data.datasets.push({
+      fill: false,
+      lineTension: 0.1,
+      borderColor: COLORS.selected,
+      borderWidth: 4,
+      pointRadius: 2,
+      pointBorderWidth: 1,
+      pointBackgroundColor: "#242a3c",
+      data: []
+    });
+
+    for (let i = 1; i <= 46; i++) {
+      config.data.datasets.push({
+        fill: false,
+        lineTension: 0.1,
+        borderColor: "#267",
+        borderWidth: 1,
+        pointRadius: 0,
+        data: []
+      });
+    }
+
+    rows.forEach(function(row, i){
+      if (switchValue === "total") {
+        config.data.labels.push(row[1] + "/" + row[2]);
+        config.data.datasets[0].data.push(row[parseInt(prefCode) + 2]);
+        for (let j = 1; j <= 46; j++) {
+          let k = (j >= parseInt(prefCode)) ? j + 1: j;
+          config.data.datasets[j].data.push(row[k + 2]);
+        }
+      } else {
+        if (i >= 1) {
+          config.data.labels.push(row[1] + "/" + row[2]);
+
+          let prev = rows[i - 1][parseInt(prefCode) + 2];
+          config.data.datasets[0].data.push(row[parseInt(prefCode) + 2] - prev);
+
+          for (let j = 1; j <= 46; j++) {
+            let k = (j >= parseInt(prefCode)) ? j + 1: j;
+            let prev = rows[i - 1][k + 2];
+            config.data.datasets[j].data.push(row[k + 2] - prev);
+          }
         }
       }
     });
@@ -469,26 +662,54 @@ const init = () => {
   }
 
   const loadData = () => {
-    $.getJSON("https://raw.githubusercontent.com/kaz-ogiwara/covid19/master/data/data.json", function(data){
+    //$.getJSON("https://raw.githubusercontent.com/kaz-ogiwara/covid19/master/data/data.json", function(data){
+    $.getJSON("data/data202004061527.json", function(data){
       gData = data;
-      drawTransitionBlocks();
+      updateThresholds();
+      drawTransitionBoxes();
       drawDemographyChart();
       drawJapanMap();
       drawRegionChart("");
+      drawPrefectureCharts("13");
       showUpdateDates();
       $("#container").addClass("show");
     })
   }
 
   const bindEvents = () => {
-    $(".switch").on("click",function(){
+    $(".transition").find(".switch").on("click",function(){
+      let $box = $(this).closest(".transition");
       $(this).siblings(".switch").removeClass("selected");
       $(this).addClass("selected");
-      drawTransitionChart($(this).closest(".transition-block"), $(this).closest(".transition-block").attr("code"));
+      drawTransitionChart($box, $box.attr("code"));
+    });
+
+    $(".prefecture-chart").find(".switch").on("click",function(){
+      let $box = $(this).closest(".prefecture-chart");
+      $(this).siblings(".switch").removeClass("selected");
+      $(this).addClass("selected");
+      drawPrefectureChart($("#select-prefecture").val(), $box.attr("code"));
+    });
+
+    $("#select-prefecture").on("change", function(){
+      let prefCode = $(this).val();
+      drawPrefectureCharts(prefCode);
+    });
+
+    $("#select-pref-type").on("change", function(){
+      drawJapanMap();
+      drawRegionChart("");
     });
 
     $(".more").on("click",function(){
       $(this).closest("p.notes").addClass("show");
+    });
+
+    $('a[href^="#"]').click(function() {
+      let href = $(this).attr("href");
+      let position = $(href).offset().top;
+      $('body,html').animate({scrollTop: position}, 400, 'swing');
+      return false;
     });
   }
 
