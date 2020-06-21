6671d2516444de0640361fb3c3c30e99e593e1de
荻原 和樹 / Kazuki OGIWARA <kaz-ogiwara@users.noreply.github.com>
2020-06-21 23:08:39 +0900

    Show chart latest date with month name
    ex. 6/21 -> 21 June

--- js/script.js
+++ js/script.js
@@ -139,12 +139,18 @@ const init = () => {
   }
 
   const drawLastDate = ($box, config) => {
+    const getDateWithMonthName = (dateString) => {
+      let dates = dateString.split("/");
+      let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+      return dates[1] + " " + months[parseInt(dates[0]) - 1];
+    }
+
     let $updated = $box.find("h5.updated");
     if (!$updated.hasClass("show")) {
       let lastDate = config.data.labels[config.data.labels.length - 1];
       let updatedDate = {
         ja: lastDate.replace("/", "月") + "日時点",
-        en: "As of " + lastDate
+        en: "As of " + getDateWithMonthName(lastDate)
       };
 
       $updated.text(updatedDate[LANG]);
@@ -756,6 +762,7 @@ const init = () => {
 
   const showUpdateDate = () => {
     $(".updated-last").text(gData.updated.last[LANG]);
+    $(".updated-demography").text(gData.updated.demography[LANG]);
   }
 
   const loadData = () => {
